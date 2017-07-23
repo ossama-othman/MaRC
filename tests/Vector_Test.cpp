b08a0592c44b6eb93ff0f79c9704e21ec569a55c
Ossama Othman <ossama.othman@gmail.com>
2017-07-23 08:06:58 -0700

    Expanded MaRC Vector and Matrix tests.
    Greatly expanded MaRC Vector and Matrix tests revealed some problems
    with the new Vector and Matrix implementations.  All problems have
    been corrected.

--- tests/Vector_Test.cpp
+++ tests/Vector_Test.cpp
@@ -18,14 +18,87 @@
 
 #include <MaRC/Vector.h>
 
+bool test_vector_initialization()
+{
+    using vector_type = MaRC::Vector<int, 3>;
+
+    vector_type const v1;  // Default initialize all elements to 0.
+
+    vector_type::value_type const n[] = {2, 3, 5};
+    std::initializer_list<vector_type::value_type>
+    vector_initializer_list{n[0], n[1], n[2]};
+
+    vector_type const v2{vector_initializer_list};
+
+    vector_type const v3{v2};
+
+    vector_type const v4 = vector_initializer_list;
+
+    vector_type const v5 = v3;
+
+    return
+        std::find_if_not(
+            std::cbegin(v1),
+            std::cend(v1),
+            [](auto value)
+            {
+                return value == vector_type::value_type();
+            }) == std::cend(v1)
+        && std::equal(std::cbegin(v2), std::cend(v2),
+                      std::cbegin(n),  std::end(n))
+        && std::equal(std::cbegin(v3), std::cend(v3),
+                      std::cbegin(v2),  std::end(v2))
+        && std::equal(std::cbegin(v4), std::cend(v4),
+                      std::cbegin(n),  std::end(n))
+        && std::equal(std::cbegin(v5), std::cend(v5),
+                      std::cbegin(v3),  std::end(v3));
+}
+
+bool test_vector_comparison()
+{
+    using vector_type = MaRC::Vector<int, 3>;
+
+    vector_type const v1{{2, 3, 5}};
+    vector_type const v2{v1};
+    vector_type const v3 = v2;
+    vector_type const v4{{7, 11, 13}};
+
+    return v2 == v1 && v3 == v2 && v4 != v1;
+}
+
+bool test_vector_addition()
+{
+    using vector_type = MaRC::Vector<int, 3>;
+
+    vector_type const v1 {{ 2, 3, 5}};
+    vector_type const v2 {{-1, 4, 0}};
+    vector_type v3{v1};
+    v3 += v2;
+
+    vector_type const sum{{ 1, 7, 5 }};
+
+    return v3 == sum && v1 + v2 == sum;
+}
+
+bool test_vector_subtraction()
+{
+    using vector_type = MaRC::Vector<int, 3>;
+
+    vector_type const v1 {{ 2, 3, 5}};
+    vector_type const v2 {{-1, 4, 0}};
+    vector_type v3{v1};
+    v3 -= v2;
+
+    vector_type const diff{{ 3, -1, 5 }};
+
+    return v3 == diff && v1 - v2 == diff;
+}
 
 int main()
 {
-  using test_vector_type = MaRC::Vector<int, 3>;
-  test_vector_type const v1{  0,  1,  5 };
-  test_vector_type const v2{  1,  2,  3 };
-  test_vector_type const s {  1,  3,  8 };
-  test_vector_type const d { -1, -1,  2 };
-
-  return v1 + v2 == s && v1 - v2 == d ? 0 : -1;
+    return
+        test_vector_initialization()
+        && test_vector_comparison()
+        && test_vector_addition()
+        && test_vector_subtraction() ? 0 : -1;
 }
