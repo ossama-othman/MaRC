44e1b38e73c7858450efa6d6e01240b9ab891761
Ossama Othman <ossama.othman@gmail.com>
2018-01-12 13:11:59 -0800

    root_find: Updated copyright years.

--- lib/MaRC/root_find.h
+++ lib/MaRC/root_find.h
@@ -4,7 +4,7 @@
  *
  * MaRC root finding related functions
  *
- * Copyright (C) 2004, 2017  Ossama Othman
+ * Copyright (C) 2004, 2017-2018  Ossama Othman
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
