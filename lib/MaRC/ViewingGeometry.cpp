77087309eee7a8d2dba6c27bf8a80e84a3544601
Ossama Othman <ossama.othman@gmail.com>
2018-01-29 14:31:15 -0800

    ViewingGeometry: Use spdlog for debug logging.

--- lib/MaRC/ViewingGeometry.cpp
+++ lib/MaRC/ViewingGeometry.cpp
@@ -28,6 +28,9 @@
 #include "Validate.h"
 #include "NullGeometricCorrection.h"
 #include "Log.h"
+#include "config.h"  // For NDEBUG
+
+#include <spdlog/fmt/ostr.h>
 
 #include <cmath>
 #include <limits>
@@ -404,44 +407,45 @@ MaRC::ViewingGeometry::rot_matrices(DVector const & range_o,
     // transformation matrices are orthogonal.
     body2observ = MaRC::transpose(observ2body);
 
-#ifdef DEBUG
+#ifndef NDEBUG
     for (std::size_t i = 0; i < 3; ++i)
-        MaRC::debug(..
-         std::cout
-            << std::abs((this->range_b_[i] - (observ2body * range_o)[i])
-                        / (this->range_b_[i] + (observ2body * range_o)[i])
-                        * 2)
-            << '\n';
+        MaRC::debug(
+            std::abs((this->range_b_[i] - (observ2body * range_o)[i])
+                     / (this->range_b_[i] + (observ2body * range_o)[i])
+                     * 2));
 
-    std::cout
-        << "range_b_ = " << this->range_b_ << "\n"
-        "range_b_ from transformation = " << observ2body * range_o
-        << "\n"
-        "range_o = " << range_o << "\n"
-        "range_o from transformation = " << body2observ * this->range_b_
-        << '\n';
+    MaRC::debug("range_b_ = {}\n"
+                "range_b_ from transformation = {}\n"
+                "range_o = {}\n"
+                "range_o from transformation = {}",
+                this->range_b_,
+                observ2body * range_o,
+                range_o,
+                body2observ * this->range_b_);
 
-    std::cout
-        << "position_angle_ = "
-        << this->position_angle_ / C::degree
-        << " degrees (positive is CCW)\n"
-        "SubLatMod = " << SubLatMod / C::degree << "\n"
-        "Ztwist = " << Ztwist / C::degree << '\n';
+    MaRC::debug("position_angle_ = {} degrees (positive is CCW)\n"
+                "SubLatMod = {}\n"
+                "Ztwist = {}\n",
+                this->position_angle_ / C::degree,
+                SubLatMod / C::degree,
+                Ztwist / C::degree);
 
-    DVector test;
-    test[2] = 1; // Unit vector along z-axis
-    DVector result(body2observ * test); // Test vector in camera
-                                            // coordinates.
+    // North pole unit vector.
+    DVector const body_north {0, 0, 1};
 
-    std::cout
-        << "Computed NORAZ = "
-        << std::atan2(-result[0], result[2]) / C::degree
-        << " degrees (positive is CCW)\n"
-        "Computed North pole vector in camera space = " << result
-        << "\n"
-           "observ2body = " << observ2body << "\n"
-           "body2observ = " << body2observ << '\n';
-#endif
+    // North pole vector in camera coordinates.
+    DVector const camera_north(body2observ * body_north);
+
+    MaRC::debug("Computed NORAZ = {} degrees (positive is CCW)\n"
+                "Computed North pole vector in camera space = {}\n"
+                "observ2body = {}\n"
+                "body2observ = {}\n",
+                std::atan2(-camera_north[0],
+                           camera_north[2]) / C::degree,
+                camera_north,
+                observ2body,
+                body2observ);
+#endif  // !NDEBUG
 
     return true;
 }
@@ -523,9 +527,9 @@ MaRC::ViewingGeometry::rot_matrices(DVector const & range_b,
         * (Geometry::RotXMatrix(SubLatMod[1])
            * Geometry::RotYMatrix(-this->position_angle_));
 
-#ifdef DEBUG
+#ifndef NDEBUG
     double Ztwist, SubLatModified;
-#endif  /* DEBUG */
+#endif  /* !NDEBUG */
 
     if (diff_magnitude > MaRC::magnitude(OA_O
                                          - o2b * UnitOpticalAxis)) {
@@ -533,13 +537,13 @@ MaRC::ViewingGeometry::rot_matrices(DVector const & range_b,
             MaRC::magnitude(OA_O - o2b * UnitOpticalAxis);
         observ2body = o2b;
 
-#ifdef DEBUG
+#ifndef NDEBUG
         SubLatModified = SubLatMod[1];
         Ztwist = Ztwist2;
     } else {
         SubLatModified = SubLatMod[0];
         Ztwist = Ztwist1;
-#endif  /* DEBUG */
+#endif  /* !NDEBUG */
     }
 
     double const percent_diff =
@@ -565,7 +569,7 @@ MaRC::ViewingGeometry::rot_matrices(DVector const & range_b,
     // transformation matrices are orthogonal.
     body2observ = MaRC::transpose(observ2body);
 
-#ifdef DEBUG
+#ifndef NDEBUG
     for (std::size_t i = 0; i < 3; ++i)
         MaRC::debug(
             "{}",
@@ -575,25 +579,36 @@ MaRC::ViewingGeometry::rot_matrices(DVector const & range_b,
 
     MaRC::debug("UnitOpticalAxis = {}\n"
                 "OpticalAxis from transformation = {}\n"
-                <<  << "\n"
-                "OA_O = {}\n" <<  << "\n"
+                "OA_O = {}\n"
                 "OA_O from transformation = {}\n",
                 UnitOpticalAxis,
                 body2observ * UnitOpticalAxis,
                 OA_O,
                 observ2body * OA_O);
 
-//   output_ << "position_angle_ = " << position_angle_ / C::degree << " degrees (positive is CCW)\n"
-//       << "SubLatModified = " << SubLatModified / C::degree << '\n'
-//       << "Ztwist = " << Ztwist / C::degree << '\n';
-//   test[2] = 1; // Unit vector along z-axis
-//   result = body2observ * test; // test vector in camera coordinates.
-//   output_ << "Computed NORAZ = " << std::atan2(-result[0], result[2]) / C::degree
-//       << " degrees (positive is CCW)\n"
-//       << "Computed North pole vector in camera space = " << result << '\n';
-//   output_ << "observ2body = " << observ2body << '\n'
-//       << "body2observ = " << body2observ << '\n';
-#endif
+    MaRC::debug("position_angle_ = {} degrees (positive is CCW)\n"
+                "SubLatModified = {}\n"
+                "Ztwist = {}\n",
+                position_angle_ / C::degree,
+                SubLatModified / C::degree,
+                Ztwist / C::degree);
+
+    // North pole unit vector.
+    DVector const body_north {0, 0, 1};
+
+    // North pole vector in camera coordinates.
+    DVector const camera_north(body2observ * body_north);
+
+    MaRC::debug("Computed NORAZ = {} degrees (positive is CCW)\n"
+                "Computed North pole vector in camera space = {}\n"
+                "observ2body = {}\n"
+                "body2observ = {}\n",
+                std::atan2(-camera_north[0],
+                           camera_north[2]) / C::degree,
+                camera_north,
+                observ2body,
+                body2observ);
+#endif  // !NDEBUG
 }
 
 void
