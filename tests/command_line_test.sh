684ca70ce724b142e647c13e0aa6ebea168c3fd0
Ossama Othman <ossama.othman@gmail.com>
2018-07-05 14:28:35 -0700

    Added test for MaRC command line options.

--- tests/command_line_test.sh
+++ tests/command_line_test.sh
@@ -1,7 +1,5 @@
 #! /bin/sh
 
-set -e
-
 # Set $top_builddir default if it was not set in the environment.
 if test -z "${top_builddir+set}"; then
   top_builddir=..
@@ -12,21 +10,35 @@ if test -z "${top_srcdir+set}"; then
   top_srcdir=..
 fi
 
+# Defined in <sysexits.h>
+EX_USAGE=64
+
 marc=$top_builddir/src/marc
 # No command line arguments.  Short usage message should be dumped.
-$marc
+$marc > /dev/null 2>&1
+test $? -eq $EX_USAGE || exit 1
 
 # Usage message.  Full usage message dumped.
-$marc --usage
+$marc --usage > /dev/null
+test $? -eq 0 || exit 1
 
 # Help message.  Full command line argument details dumped.
-$marc --help
-$marc -?
+$marc --help > /dev/null
+test $? -eq 0 || exit 1
+
+$marc -? > /dev/null
+test $? -eq 0 || exit 1
 
 # Version message.  Dump MaRC program version.
-$marc --version
-$marc -V
+$marc --version > /dev/null
+test $? -eq 0 || exit 1
+
+$marc -V > /dev/null
+test $? -eq 0 || exit 1
 
 # Explictly mark end of command line options.
-$marc --
-$marc -- foo
+$marc -- > /dev/null 2>&1
+test $? -eq $EX_USAGE || exit 1
+
+$marc -- foo > /dev/null 2>&1
+test $? -ne 0 || exit 1
