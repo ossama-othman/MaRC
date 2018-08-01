1c9437ad881b68205c3073c7d3002145d68f2d34
Ossama Othman <ossama.othman@gmail.com>
2018-08-01 12:48:16 -0700

    src: Silence unused argument warning.

--- src/PhotoImageFactory.cpp
+++ src/PhotoImageFactory.cpp
@@ -60,7 +60,7 @@ MaRC::PhotoImageFactory::PhotoImageFactory(char const * filename)
 }
 
 std::unique_ptr<MaRC::SourceImage>
-MaRC::PhotoImageFactory::make(scale_offset_functor calc_so)
+MaRC::PhotoImageFactory::make(scale_offset_functor /* calc_so */)
 {
     if (!this->config_ || !this->geometry_)
         return nullptr;  // not set or make() already called!
