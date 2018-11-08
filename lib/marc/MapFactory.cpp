c7a22b576b50d1c539f7090f741827acd706bb13
Ossama Othman <ossama.othman@gmail.com>
2018-11-07 21:52:31 -0800

    lib: Added note about grid interval validation.

--- lib/marc/MapFactory.cpp
+++ lib/marc/MapFactory.cpp
@@ -30,6 +30,18 @@ MaRC::MapFactory::make_grid(std::size_t samples,
                             double lat_interval,
                             double lon_interval)
 {
+    /**
+     * @todo Verify that @a lat_interval falls within the maximum
+     *       latitude range in degrees, i.e. the half-open interval
+     *       [0, 180) (always positive!), and similarly for the
+     *       @c lon_interval and the longitude range [0, 360).  The
+     *       values should be large enough to ensure that data is
+     *       always "behind" the grid, otherwise. MaRC and the MaRC
+     *       library should not allow the invalid lat/lon grid
+     *       intervals to be set to begin with.  Such inputs should be
+     *       validated beyond the existing negative value checks.
+     */
+
     grid_type grid(samples * lines, grid_type::value_type());
 
     this->plot_grid(samples,
