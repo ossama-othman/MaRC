d9552dbef89de9c3d6d6718982113eb2afc3bc5e
Ossama Othman <ossama.othman@gmail.com>
2018-11-27 14:50:37 -0800

    lib: Generalize image inversion functions.
    Generalize the MaRC::invert_samples() and MaRC::invert_lines()
    functions so that they work on any STL style container with the
    appropriate iterators, as well as basic arrays.

--- lib/marc/utility.h
+++ lib/marc/utility.h
@@ -27,13 +27,43 @@
 #ifndef MARC_UTILITY_H
 #define MARC_UTILITY_H
 
-#include <vector>
+#include <iterator>
 #include <algorithm>
 #include <stdexcept>
 
 
 namespace MaRC
 {
+#if __cplusplus < 201703L
+    /**
+     * @brief Get the size of a container.
+     *
+     * @deprecated This is an implementation of the C++17
+     *             @c std::size() function.  It will be removed when
+     *             %MaRC requires C++17.
+     */
+    template <class C>
+    constexpr auto size(C const & c) -> decltype(c.size())
+    {
+        return c.size();
+    }
+
+    /**
+     * @brief Get the length of an array.
+     *
+     * @deprecated This is an implementation of the C++17
+     *             @c std::size() function.  It will be removed when
+     *             %MaRC requires C++17.
+     */
+    template <class T, std::size_t N>
+    constexpr std::size_t size(T const (&array)[N]) noexcept
+    {
+        return N;
+    }
+#else
+    using std::size;
+#endif  // __cplusplus < 201703L
+
     /**
      * @brief Invert image samples (columns).
      *
@@ -41,7 +71,8 @@ namespace MaRC
      * reflected from left to right about the center column.
      *
      * @param[in,out] image   Image to be inverted from left to
-     *                        right.
+     *                        right.  This should be an array or
+     *                        container.
      * @param[in]     samples Number of columns in the @a image.
      * @param[in]     lines   Number of rows    in the @a image.
      *
@@ -50,15 +81,15 @@ namespace MaRC
      */
     template <typename T>
     void
-    invert_samples(std::vector<T> & image,
+    invert_samples(T & image,
                    std::size_t samples,
                    std::size_t lines)
     {
-        if (image.size() != samples * lines)
+        if (MaRC::size(image) != samples * lines)
             throw std::invalid_argument("Image size does not match "
                                         "number of samples and lines.");
 
-        auto begin = image.begin();
+        auto begin = std::begin(image);
         for (std::size_t line = 0; line < lines; ++line) {
             auto const end = begin + samples;
             std::reverse(begin, end);
@@ -73,7 +104,8 @@ namespace MaRC
      * from top to bottom about the center row.
      *
      * @param[in,out] image   Image to be inverted from top to
-     *                        bottom.
+     *                        bottom.  This should be an array or
+     *                        container.
      * @param[in]     samples Number of columns in the @a image.
      * @param[in]     lines   Number of rows    in the @a image.
      *
@@ -82,11 +114,11 @@ namespace MaRC
      */
     template <typename T>
     void
-    invert_lines(std::vector<T> & image,
+    invert_lines(T & image,
                  std::size_t samples,
                  std::size_t lines)
     {
-        if (image.size() != samples * lines)
+        if (MaRC::size(image) != samples * lines)
             throw std::invalid_argument("Image size does not match "
                                         "number of samples and lines.");
 
@@ -94,12 +126,12 @@ namespace MaRC
 
         for (std::size_t line = 0; line < middle; ++line) {
             // Line from one end.
-            auto const top_begin = image.begin() + line * samples;
+            auto const top_begin = std::begin(image) + line * samples;
             auto const top_end   = top_begin + samples;
 
             // Line from the other end.
             std::size_t const offset = (lines - line - 1) * samples;
-            auto const bottom_begin = image.begin() + offset;
+            auto const bottom_begin = std::begin(image) + offset;
 
             // Swap the lines.
             std::swap_ranges(top_begin, top_end, bottom_begin);
