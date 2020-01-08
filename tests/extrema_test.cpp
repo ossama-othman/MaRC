a4bee95aaea4f1adb9da545e32558a3fae7086b6
Ossama Othman <ossama.othman@gmail.com>
2020-01-07 22:00:05 -0800

    tests: Expand MaRC::extrema<> test.

--- tests/extrema_test.cpp
+++ tests/extrema_test.cpp
@@ -14,16 +14,18 @@
 
 namespace
 {
+    /// Primary extremum type alias used by the overall test.
     using extremum_type = int;
 }
 
+/**
+ * @test Test the MaRC::extrema<> handling of bad values.
+ */
 template <typename T>
 bool test_bad_extrema(T minimum, T maximum)
 {
     try {
         MaRC::extrema<int> e(minimum, maximum);
-
-        return !e.is_valid();  // Unreached
     } catch(std::invalid_argument &) {
         return true;
     } catch(...) {
@@ -33,6 +35,9 @@ bool test_bad_extrema(T minimum, T maximum)
     return false;
 }
 
+/**
+ * @test Test the MaRC::extrema<> initialization.
+ */
 bool test_initialization()
 {
     constexpr extremum_type good_min = -1;
@@ -52,36 +57,105 @@ bool test_initialization()
     MaRC::extrema<int> e1;
     MaRC::extrema<int> e2(good_min, good_max);
     MaRC::extrema<int> e3(d_min, d_max);
+    MaRC::extrema<int> e4(good_min, good_min);
 
     return !e1.is_valid()
         && e2.is_valid()
         && e3.is_valid()
+        && e4.is_valid()
 
         && test_bad_extrema(bad_min, bad_max)
         && test_bad_extrema(std::numeric_limits<double>::quiet_NaN(),
                             d_max)
 
+        && e2.minimum() == good_min
+        && e2.maximum() == good_max
+
         && e3.minimum() == std::numeric_limits<extremum_type>::lowest()
-        && e3.maximum() == std::numeric_limits<extremum_type>::max();
+        && e3.maximum() == std::numeric_limits<extremum_type>::max()
+
+        && e4.minimum() == good_min
+        && e4.minimum() == e4.maximum();
 }
 
+/**
+ * @test Test the MaRC::extrema<>::update() from datum.
+ */
+bool test_update(MaRC::extrema<extremum_type> & e,
+                 extremum_type datum,
+                 extremum_type expected_minimum,
+                 extremum_type expected_maximum)
+{
+    e.update(datum);
+
+    return e.is_valid()
+        && e.minimum() == expected_minimum
+        && e.maximum() == expected_maximum;
+}
+
+/**
+ * @test Test the MaRC::extrema<>::update() from other extrema<>.
+ */
+bool test_update(MaRC::extrema<extremum_type> & to,
+                 MaRC::extrema<extremum_type> const & from,
+                 extremum_type expected_minimum,
+                 extremum_type expected_maximum)
+{
+    to.update(from);
+
+    return to.is_valid()
+        && to.minimum() == expected_minimum
+        && to.maximum() == expected_maximum;
+}
+
+/**
+ * @test Test the MaRC::extrema<>::update() methods.
+ */
 bool test_update()
 {
     constexpr extremum_type a = -1000;
     constexpr extremum_type b = -100;
-    constexpr extremum_type c = a;
+    constexpr extremum_type c =  0;
+    constexpr extremum_type d =  10;
 
-    MaRC::extrema<extremum_type> e1(a, b);
-    MaRC::extrema<extremum_type> e2(a, c);
+    MaRC::extrema<extremum_type> e1;
+    MaRC::extrema<extremum_type> e2(b, c);
+    MaRC::extrema<extremum_type> e3(a, c);
+    MaRC::extrema<extremum_type> e4(b, d);
+    MaRC::extrema<extremum_type> e5(b, c);
 
-    return e1.is_valid()
-        && e2.is_valid()
+    return test_update(e1, b, b, b)  // First update
+        && test_update(e1, b, b, b)  // No change
+        && test_update(e1, a, a, b)  // Minimum updated
+        && test_update(e1, b, a, b)  // No change
+        && test_update(e1, d, a, d)  // Maximum updated
+        && test_update(e1, c, a, d)  // No change
 
-        && e1.minimum() == a
-        && e1.maximum() == b
-        && e2.minimum() == e2.maximum();
+        && test_update(e1,            // "a" and "d"
+                       e1,            // "b" and "c"
+                       e1.minimum(),  // No change
+                       e1.maximum())  // No change
+        && test_update(e1,            // "a" and "d"
+                       e2,            // "b" and "c"
+                       e1.minimum(),
+                       e1.maximum())  // No change
+        && test_update(e2,            // "b" and "c"
+                       e3,            // "a" and "c"
+                       e3.minimum(),
+                       e2.maximum())  // e2 minimum updated
+        && test_update(e3,            // "a" and "c"
+                       e4,            // "b" and "d"
+                       e3.minimum(),
+                       e4.maximum())  // e3 maximum updated
+        && test_update(e5,            // "b" and "c"
+                       e3,            // "a" and "d"
+                       e3.minimum(),
+                       e3.maximum()); // e5 minimum and maximum updated
 }
 
+/**
+ * @test Test the MaRC::extrema<>::reset() method.
+ */
 bool test_reset()
 {
     constexpr extremum_type a = -1000;
@@ -95,7 +169,6 @@ bool test_reset()
 
     return !e.is_valid();
 }
-
 
 /// The canonical main entry point.
 int main()
