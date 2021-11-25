a0237852494f3a785c852f78ffd1930687ad1f14
Ossama Othman <ossama.othman@gmail.com>
2021-11-24 22:52:37 -0800

    unweighted_average: Perform average as needed.
    Prevent floating point errors from being introduced by only
    calculating the average if more than one image contributed to the
    composited data.

--- lib/marc/unweighted_average.cpp
+++ lib/marc/unweighted_average.cpp
@@ -51,7 +51,13 @@ MaRC::unweighted_average::composite(list_type const & images,
         }
     }
 
-    if (count > 0) {
+    /*
+      Perform the average only if more than one image contributed to
+      avoid introducing floating point error, such as a datum of 200
+      for one image vs. 199.999999999996 obtained from the average
+      calculation.
+    */
+    if (count > 1) {
         // Calculate the average.
         data = static_cast<double>(sum / count);
     }
